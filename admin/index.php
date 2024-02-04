4a936771b63a5cbb23bb998f60793a32bb926346
Masirika Constantin <105310695+MasirikaConstantin@users.noreply.github.com>
2024-02-04 15:44:32 +0100

    Add files via upload

---
+++ admin/index.php
@@ -0,0 +1,114 @@
+<?php
+$titre="Accueil";
+
+
+
+
+  require('utilitaire/heade.php');
+
+?>
+<!-- Begin page content -->
+<main class="flex-shrink-0 mt-5 ">
+  <div class="container">
+    <h1 class="mt-5">Forum Actualités</h1>
+    <p class="lead">Bienvenue sur notre plate-forme de questions et réponses, si vous avez une question n'hésité pas à la pauser et si vous avez une réponse à une éventuelle question votre réponse nous sera d'une grande utilité</p>
+    <div class="container mb-7">
+
+
+  <div class="my-3 p-3 bg-body rounded shadow-sm">
+    <h6 class="border-bottom pb-2 mb-0">Recent updates</h6>
+    <div class="d-flex text-muted pt-3">
+      <svg class="bd-placeholder-img flex-shrink-0 me-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 32x32" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#007bff"/><text x="50%" y="50%" fill="#007bff" dy=".3em">32x32</text></svg>
+
+      <p class="pb-3 mb-0 small lh-sm border-bottom">
+        <strong class="d-block text-gray-dark">@username</strong>
+        Some representative placeholder content, with some information about this user. Imagine this being some sort of status update, perhaps?
+      </p>
+    </div>
+    <div class="d-flex text-muted pt-3">
+      <svg class="bd-placeholder-img flex-shrink-0 me-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 32x32" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#e83e8c"/><text x="50%" y="50%" fill="#e83e8c" dy=".3em">32x32</text></svg>
+
+      <p class="pb-3 mb-0 small lh-sm border-bottom">
+        <strong class="d-block text-gray-dark">@username</strong>
+        Some more representative placeholder content, related to this other user. Another status update, perhaps.
+      </p>
+    </div>
+    <div class="d-flex text-muted pt-3">
+      <svg class="bd-placeholder-img flex-shrink-0 me-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 32x32" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#6f42c1"/><text x="50%" y="50%" fill="#6f42c1" dy=".3em">32x32</text></svg>
+
+      <p class="pb-3 mb-0 small lh-sm border-bottom">
+        <strong class="d-block text-gray-dark">@username</strong>
+        This user also gets some representative placeholder content. Maybe they did something interesting, and you really want to highlight this in the recent updates.
+      </p>
+    </div>
+    <small class="d-block text-end mt-3">
+      <a href="#">All updates</a>
+    </small>
+  </div>
+
+
+
+
+  
+
+
+    <div class="row mb-2">
+    <div class="col-md-6">
+      <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+        <div class="col p-4 d-flex flex-column position-static">
+          <strong class="d-inline-block mb-2 text-primary">World</strong>
+          <h3 class="mb-0">Featured post</h3>
+          <div class="mb-1 text-muted">Nov 12</div>
+          <p class="card-text mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
+          <a href="#" class="stretched-link">Continue reading</a>
+        </div>
+        <div class="col-auto d-none d-lg-block">
+          <svg class="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+
+        </div>
+      </div>
+    </div>
+    <div class="col-md-6">
+      <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+        <div class="col p-4 d-flex flex-column position-static">
+          <strong class="d-inline-block mb-2 text-success">Design</strong>
+          <h3 class="mb-0">Post title</h3>
+          <div class="mb-1 text-muted">Nov 11</div>
+          <p class="mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
+          <a href="#" class="stretched-link">Continue reading</a>
+        </div>
+        <div class="col-auto d-none d-lg-block">
+          <svg class="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+
+
+
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eos porro quia placeat eligendi eius laboriosam officia quas nisi excepturi suscipit nobis aspernatur voluptas temporibus ullam, dolorum ipsam vitae. Cum, repellendus?
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis consequatur est maxime cupiditate, totam enim sit? Facilis temporibus atque quo veritatis provident, eligendi officiis quia cumque qui quidem, blanditiis necessitatibus.
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Totam temporibus nam itaque perferendis saepe ipsam quasi voluptates amet deleniti eius! Nostrum eos ab asperiores animi quas dolores eveniet nihil quod?
+
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eos porro quia placeat eligendi eius laboriosam officia quas nisi excepturi suscipit nobis aspernatur voluptas temporibus ullam, dolorum ipsam vitae. Cum, repellendus?
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis consequatur est maxime cupiditate, totam enim sit? Facilis temporibus atque quo veritatis provident, eligendi officiis quia cumque qui quidem, blanditiis necessitatibus.
+    met consectetur, adipisicing elit. Totam temporibus nam itaque perferendis saepe ipsam quasi voluptates amet deleniti eius! Nostrum eos ab asperiores animi quas dolores eveniet nihil quod?
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eos porro quia placeat eligendi eius laboriosam officia quas nisi excepturi suscipit nobis aspernatur voluptas temporibus ullam, dolorum ipsam vitae. Cum, repellendus?
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis consequatur est maxime cupiditate, totam enim sit? Facilis temporibus atque quo veritatis provident, eligendi officiis quia cumque qui quidem, blanditiis necessitatibus.
+      Lorem ip
+    </div>
+    <p>Back to <a href="/docs/5.3/examples/sticky-footer/">the default sticky footer</a> minus the navbar.</p>
+  </div>
+</main>
+
+<?php
+
+
+
+
+  require('utilitaire/foot.php');
+
+?>
---
+++ admin/utilitaire/foot.php
@@ -0,0 +1,28 @@
+
+<style>
+    .s{
+    
+      margin: .15rem;
+      font-size: 1.5rem;
+    
+    }
+</style>
+<div class="container">
+  <footer class="py-5">
+
+    <div class="d-flex justify-content-between py-4 my-4 border-top">
+      <p>&copy; 2021 Company, Inc. All rights reserved.</p>
+      <ul class="list-unstyled d-flex">
+        <li class="ms-3"><a class="link-white" data-bs-toggle="tooltip" data-bs-placement="top" title="Constantin Masirika Jr." href="#">  <i class="bi bi-facebook s"></i></a></li>
+        <li class="ms-3"><a class="link-white" data-bs-toggle="tooltip" data-bs-placement="top" title="Constantin Masirika Jr." href="#">  <i class="bi bi-twitter s"></i></a></li>
+        <li class="ms-3"><a class="link-white" data-bs-toggle="tooltip" data-bs-placement="top" title="Constantin Masirika Jr." href="#"> <i class="bi bi-instagram s"></i></a></li>
+      </ul>
+    </div>
+<script src="/css/dist/js/bootstrap.bundle.min.js" ></script>
+
+  </footer>
+</div>
+
+  
+    </body>
+</html>
