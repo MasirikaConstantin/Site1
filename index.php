821992a673cea6a71446e213b9ce24028c9690f3
Masirika Constantin <105310695+MasirikaConstantin@users.noreply.github.com>
2024-02-04 15:48:22 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="canonical" href="users/index.php">
+    <meta name="robots" content="noindex">
+    <meta http-equiv="refresh" content="0; url=users/index.php">
+  
+</head>
